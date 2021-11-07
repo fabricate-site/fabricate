2048b78b7503d0514f7873d180e0c0f6de1db915
Andrew Foltz-Morrison <afoltzm@gmail.com>
2021-11-07 18:51:33 -0500

    A less ambiguous grammar

--- src/site/fabricate/prototype/read/grammar.clj
+++ src/site/fabricate/prototype/read/grammar.clj
@@ -3,21 +3,22 @@
 
 (def form
   (insta/parser
-   "form = EPSILON | ( expr | #'(?:[^✳.*🔚]*)' | form)*
-    expr = ('✳=' | '✳') #'[^🔚]*' '🔚'"))
+   "form = EPSILON | ( expr | txt )*
+    txt = #'(?:[^✳🔚]*)'
+    expr = #'✳=?' #'[^🔚]*' '🔚'"))
 
 
 (comment
 
 
-  (re-matches  #"(?:[^✳.*🔚]*)"
+  (re-matches  #"(?:[^✳🔚]*)"
                "abc")
 
-  (re-matches  #"(?:[^✳.*🔚]*)"
+  (re-matches  #"(?:[^✳🔚]*)"
                "✳abc🔚")
 
-  (re-matches  #"(?:[^✳.*🔚]*)"
-               "✳(🔚")
+  (re-matches  #"(?:[^✳🔚]*)"
+               "something.")
 
   (form "✳=abcd🔚 some text")
 
@@ -25,6 +26,12 @@
 
   (form "✳=(my.ns/fn  22)🔚 some text")
 
+  (form "a first sentence. another sentence.")
+
+  (form "When I was on my 𝑛th refactor of a thread macro with multiple embedded if expressions, I realized I was trying to handle lots of special cases without really thinking about them. But in the back of my mind, I knew I was trying to dispatch my function calls on the basis of whether the data I was passing through my function matched some predicate or other - another way of saying that the data was in a particular ✳= [:em \"state.\"] 🔚")
+
   (form (slurp "./pages/finite-schema-machines.html.fab"))
+
+  (take 3 (insta/parses form  "When I was on my 𝑛th refactor of a thread macro with multiple embedded if expressions, I realized I was trying to handle lots of special cases without really thinking about them. But in the back of my mind, I knew I was trying to dispatch my function calls on the basis of whether the data I was passing through my function matched some predicate or other - another way of saying that the data was in a particular ✳= [:em \"state.\"] 🔚"))
 
   )
