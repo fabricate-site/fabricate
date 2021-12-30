70fed190e0543e3a51e8ec9c65fde14f96970a0e
Andrew Foltz-Morrison <afoltzm@gmail.com>
2021-12-30 10:10:07 -0500

    Extend FSM advance/complete fns to additional args
    This allows for a more parameterized execution of the functions; the
    additional arguments can be considered like "constants" where the input
    value is the "variable" as the FSM advances through the succession of states.

--- test/site/fabricate/prototype/fsm_test.clj
+++ test/site/fabricate/prototype/fsm_test.clj
@@ -17,6 +17,9 @@
    [:enum {:description "state 2"} 2] inc
    [:enum {:description "final state"} 3] identity})
 
+(def fsm-additional-args
+  {[:= {:description "state 1"} 1] +
+   [:= {:description "state 2"} 2] (constantly 2)})
 
 (t/deftest finite-schema-machines
   (t/testing "schema"
@@ -25,9 +28,14 @@
 
   (t/testing "advance"
     (t/is (= 3 (advance example-fsm 2)))
-    (t/is (= 0 (advance example-fsm 0))))
+    (t/is (= 0 (advance example-fsm 0)))
+
+    (t/is (= 2 (advance fsm-additional-args 1 1)))
+    )
 
   (t/testing "complete"
     (t/is (= 3 (complete example-fsm 1)))
     (t/is (= 3 (complete example-fsm 2)))
-    (t/is (= 0 (complete example-fsm 0)))))
+    (t/is (= 0 (complete example-fsm 0)))
+
+    (t/is (= 2 (complete fsm-additional-args 1 1)))))
