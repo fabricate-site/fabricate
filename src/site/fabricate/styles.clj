73b364b7a9e6e79b59b5b7b7e92a4a93ff7d2203
Andrew Foltz-Morrison <afoltzm@gmail.com>
2022-06-03 09:55:07 -0400

    Settling on a flexible and simple style

--- src/site/fabricate/styles.clj
+++ src/site/fabricate/styles.clj
@@ -6,69 +6,86 @@
 
 (def docs
   (list
-   (stylesheet/at-import "https://fonts.googleapis.com/css2?family=Overpass+Mono:wght@300..700&family=Overpass:ital,wght@100..700&display=swap")
-   (stylesheet/at-import "https://fonts.googleapis.com/css2?family=Spline+Sans:wght@300..700&display=swap")
-   (stylesheet/at-font-face {:font-family "Katahdin"
-                             :src "url(/media/KatahdinRound-Bold.woff2) format('woff')"})
-   (stylesheet/at-font-face
-    {:font-family "Newsreader"
-     :src "url(/media/Newsreader-Italic[opsz,wght].woff2) format('woff')"})
-   (stylesheet/at-font-face {:font-family "Newsreader"
-                             :src "url(/media/Newsreader[opsz,wght].woff2) format('woff')"})
-   [:article {:font-family "'Overpass', sans-serif"
-              :font-size "min(4vmin, 22px)"
+   (stylesheet/at-import "https://fonts.googleapis.com/css2?family=Radio+Canada:ital,wght@0,300..700;1,300..700")
+   (stylesheet/at-import "https://fonts.googleapis.com/css2?family=Spline+Sans+Mono:wght@300..700&display=swap")
+   [:article {:font-family #_"'Radio Canada', 'Overpass', sans-serif"
+              "'Roboto Flex',sans-serif"
+              :font-optical-sizing "auto"
+              :font-size "min(4vmin, 20px)"
+              :font-variation-settings "'wdth' 91"
               :color "#222"
               #_ #_ :margin-left "1vw"
               :line-height "1.29em"
               :display "grid"
-              :grid-template-columns "repeat(12, minmax(5px, 1fr))"
-              :grid-column-gap "1rem"
+              :grid-template-columns "repeat(12, minmax(3px, 1fr))"
+              :grid-column-gap "0.5rem"
               :grid-row-gap "0.5em"}]
-   [:aside {:margin-left "1.75vw" :font-size "0.8em" :font-weight "400"}]
-   [(select/aside select/before) {:content "\"🤔\"" :left "0" :position "absolute" :padding "0.5em" :margin-left "1.5vw"}]
+   [:aside {:grid-column "5 / 12"
+            :font-size "0.8em" :font-weight "400"}]
+   #_[(select/aside select/before)
+      {:content "\"📝\"" :left "0"
+       :position "absolute" :grid-column "5"}]
    [:h1 :h2 :h3 :h4 :h5 :h6
+    :header
     {:font-weight "900"
-     :font-family "'Overpass', sans-serif"
+     :grid-column "2 / 10"
+     :font-family "'Roboto Flex',sans-serif"
      :margin-bottom "0.25em"
      :margin-top "0em"}]
    [:body {:background-color "#E2DED6"}]
-   [:p {:grid-column "4 / 9"
+   [:p {:grid-column "3 / 10"
         :margin-bottom "0.5em"
         :padding "0px"
         :margin-top "0em"
         :hyphens "auto"}]
-   [:div {:grid-column "3 / 10"
+   [:div {:grid-column "2 / 12"
           :margin-bottom "0em"
           :margin-top "0em"}]
-   [:ul :ol {:grid-column "4 / 9"
+   [:ul :ol {:grid-column "3 / 11"
              :margin-top "0em"
              :margin-bottom "0em"}
-    [:li {:margin-bottom "0.5em"}]]
-  (stylesheet/at-media
-   {:max-width "750px"}
-   [:p :ul :ol :pre {:grid-column "2 / 11"}]
-   [:div {:grid-column "1 / 12"}])
-   [:code {:font-family "'Overpass Mono', monospace"
+    [:li {:margin-bottom "0.4em"
+          :margin-left "-2rem"
+          :padding-left "0em"}]]
+   [:figure :blockquote {:grid-column "3 / 12"}]
+   [:code {:font-family "'Spline Sans Mono', monospace"
+           :font-weight "500"
+           :font-size "0.92em"
+           :white-space "pre-wrap"
            :background-color "#EEE"}]
    [:pre {:background-color "#EEE" :white-space "pre-wrap"
-          :grid-column "3 / 10"
+          :grid-column "3 / 12"
           :margin-bottom "0.5em"
+          :font-weight "450"
+          :line-height "1.45rem"
           :margin-top "0em"
-          :font-family "'Overpass Mono', monospace"}
+          :font-family "'Spline Sans Mono', monospace"}
     [:ins {:text-decoration "none" :background-color "#ADC3A2"}]
     [(select/ins select/before) {:position "relative"
                                  :background-color "#EEE"
                                  :font-weight "700"
                                  :content "\"+\""}]
     [(select/ins select/after) {:content "\"\"" :display "block"}]]
-   [:.xl-text {:font-family "Katahdin, sans-serif"
+   (stylesheet/at-media
+    {:max-width "900px"}
+    [:p :ul :ol {:grid-column "2 / 11"}]
+    [:div {:grid-column "1 / 13"}]
+    [:pre :figure :blockquote {:grid-column "2 / 13"}]
+    [:h1 :h2 :h3 :h4 :h5 :h6
+     :header
+     {:grid-column "1 / 13"}])
+   [:.xl-text {:font-family "Roboto Flex, sans-serif"
+               :font-variation-settings "'wdth' 141"
+               :font-weight 1000
                :font-size "2.5em" :line-height "1em"
                :text-transform "uppercase"}]
-   [:.l-text {:font-family "Katahdin, sans-serif"
+   [:.l-text {:font-family "Roboto Flex, sans-serif"
+              :font-variation-settings "'wdth' 141"
+              :font-weight 1000
               :font-size "1.5em" :line-height "1em"
               :text-transform "uppercase"}]
    [:.language-clojure
-    {:font-family "Overpass Mono, sans-serif"}
+    {:font-family "'Spline Sans Mono', monospace"}
     [:.keyword {:color "#7f2c28"}]
     [:.string {:color "#7a7533"}]]
    [:figure {:margin-left "0.25vw"
@@ -86,8 +103,9 @@
     [(select/code select/after) {:content "\"\"" :display "block"}]]
    [:.shell [(select/code select/before) {:position "relative" :content "\"$ \""}]]
    [:.small {:font-size "0.75em"}]
-   [:.example-rows {:display "flex"
-                    :flex-flow "row wrap"
-                    :justify-content "space-between"
-                    :column-gap "1em"}]
-   [:.example-row {:flex "1 1 30ch" :font-size "0.75em"}]))
+   #_[:.example-rows {:display "flex"
+                      :flex-flow "row wrap"
+                      :justify-content "space-between"
+                      :column-gap "1em"}]
+   [:.example-row #_{:flex "1 1 30ch" :font-size "0.75em"}
+    {:grid-row "span 2"}]))
