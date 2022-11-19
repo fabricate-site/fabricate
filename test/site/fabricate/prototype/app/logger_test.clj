6f7c5d55b619efeb32fe589c5fd879d8b5579159
Andrew Foltz-Morrison <afoltzm@gmail.com>
2022-11-19 10:33:05 -0500

    [WIP] A test logging configuration that doesn't write logs
    It's not yet clear why.

--- test/site/fabricate/prototype/app/logger_test.clj
+++ test/site/fabricate/prototype/app/logger_test.clj
@@ -1,16 +1,16 @@
 (ns site.fabricate.prototype.app.logger-test
   (:require [com.brunobonacci.mulog :as u]
+            [clojure.edn :as edn]
             [site.fabricate.prototype.write :refer [default-site-settings]]
             [clojure.test :as t]
-            [clojure.java.io])
+            [clojure.java.io :as io])
   (:import [java.io File]))
-
 
 (t/deftest logger-event-capture
 
   (t/testing "ability of logger to capture and surface relevant events"
 
-    (let [test-file (File/createTempFile "test-logs" ".edn")
+    (let [test-file (File/createTempFile "test-logs-" ".edn")
           file-pub
           (u/start-publisher!
            {:type :simple-file
@@ -19,19 +19,26 @@
                                [:site.fabricate.app.logger/config
                                 :transform])})]
 
-      (u/log ::test-event)
+      (u/with-context {:log/level 800}
 
-      (println (slurp test-file))
-      (file-pub))
+        (u/log ::test-event)
 
-    )
-  )
+        (u/log ::test-low-level-event :log/level 200)
 
+        (u/trace ::test-trace {:capture (fn [r] {:output/value r})}
+                 (do (println "test event") :done)))
+
+      (let [log-data (edn/read-string
+                      (str "[" (slurp test-file) "]"))]
+
+        (t/is (not-empty log-data))
+        (t/is (= 2 (count log-data)))
+        (t/is (not-any? #(= ::test-low-level-event (:mulog/event-name %))
+                        log-data)))
+
+      (file-pub)
+      (io/delete-file test-file))))
 
 (comment
 
-
-
-  (with-out-str (u/log ::test-event :some/data 3))
-
-  )
+  (with-out-str (u/log ::test-event :some/data 3)))
