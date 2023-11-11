dc0d9c622274c41e1af56658cf48868abd6633df
Andrew Foltz-Morrison <thefomo@gmail.com>
2023-11-11 12:39:29 -0500

    Initial implementation of produce! multimethod
    Adding the :site.fabricate.file/output-file key to the entry map only on
    a successful write is a simple and effective way of distinguishing success and
    failure.
    
    The dictionary definition multimethod metadata may need to be removed to a
    secondary metadata path (see
    https://danielgregoire.dev/posts/2021-10-15-clojure-src-test-meta/), but it's
    worth recording here for the time being, as I think the dictionary definitions
    have helped clarify the concepts in my mind, and thus might be useful to users
    of Fabricate (once the documentation has been regenerated using this metadata).
    
    As expected, the implementation is straightforward when the steps are clearly delineated

--- src/site/fabricate/prototype/api.clj
+++ src/site/fabricate/prototype/api.clj
@@ -6,9 +6,12 @@
             [site.fabricate.prototype.page :as page]
             [site.fabricate.prototype.html :as html]
             [malli.core :as m]
+            [hiccup.page]
             [clojure.java.io :as io]
             [clojure.spec.alpha :as spec]
-            [babashka.fs :as fs]))
+            [babashka.fs :as fs])
+  (:import [java.io File]
+           [java.net URI]))
 
 (comment
   (def entry-schema
@@ -17,45 +20,29 @@
                [:site.fabricate/output-file [:fn #(instance? java.io.File %)]]
                [:page/title :string]])))
 
-(spec/def :site.fabricate/input-file #(instance? java.io.File %))
-(spec/def :site.fabricate/output-file #(instance? java.io.File %))
+(spec/def :site.fabricate/input-file #(instance? File %))
+(spec/def :site.fabricate/output-file #(instance? File %))
 
 (spec/def :site.fabricate/entry
   (spec/keys :req [:site.fabricate/input-file]
              :opt [:site.fabricate/output-file]))
 
-
-(def patterns)
-
 (def patterns
-  {;; "docs/**.md"
-   ;; {:input/format :markdown,
-   ;;  :page/format :hiccup,
-   ;;  :outputs [{:output/format :pdf, :output/location "pdfs/"}
-   ;;            {:output/format :html, :output/location "html/"}]},
-   "**.fab" {:input/format :fabricate/v1,
-             :page/format :hiccup,
-             :outputs [{:output/format :html, :output/location "docs"}]}
-   ;; "**.docx" {:input/format :word/docx}
-   ;; "./obsidian/website/*.md" {:input/format :markdown}
-   })
-
-(defn- output-path
-  [input-file output-location]
-  (cond (fs/directory? output-location)
-        (fs/file (str output-location "/" (fs/file-name input-file)))
-        (instance? java.io.File output-location) output-location))
+  {"**.fab" {:site.fabricate.page/input-format :fabricate/v0,
+             :site.fabricate.page/format :hiccup,
+             :site.fabricate.page/outputs
+               [{:site.fabricate.page/output-format :html,
+                 :site.fabricate.page/output-location "docs"}]}})
 
 (defn- plan-pattern-entries
   ([[pattern entry-data] input-dir]
    (for [matched-file (fs/glob input-dir pattern)
-         output (:outputs entry-data)]
-     (let [input-file (fs/file matched-file)]
-       (-> entry-data
-           (dissoc :outputs)
-           (assoc :site.fabricate/input-file input-file
-                  :site.fabricate/output output
-                  :match/pattern pattern))))))
+         output (:site.fabricate.page/outputs entry-data)]
+     (-> entry-data
+         (dissoc :site.fabricate.page/outputs)
+         (assoc :site.fabricate.page/input-file (fs/file matched-file)
+                :site.fabricate.page/output output
+                :site.fabricate.page/match-pattern pattern)))))
 
 (defn plan
   [{:keys [patterns input-dir], :as settings}]
@@ -64,22 +51,74 @@
 ;; not enforcing a spec on a multimethod seems like the best way of keeping
 ;; it open for extension, and keeps the API simpler.
 (defmulti assemble
-  "Assemble the given entry by generating structured content from the entry's input file"
-  (fn [entry] [(:input/format entry) (:page/format entry)]))
+  "Generate structured content from the entry's input file."
+  {:term/definition
+     {:source (URI. "https://www.merriam-webster.com/dictionary/assemble"),
+      :definition "to fit together the parts of"}}
+  (fn [entry] [(:site.fabricate.page/input-format entry)
+               (:site.fabricate.page/format entry)]))
 
-(defn fabricate-v1->hiccup
-  [{:keys [site.fabricate/input-file], :as entry}]
+(defn fabricate-v0->hiccup
+  "Generate a Hiccup representation of the page by evaluating the parsed Fabricate template of the page contents."
+  [{:keys [site.fabricate.page/input-file], :as entry}]
   (let [parsed-page (read/parse (slurp input-file))
-        evaluated-page (read/eval-all parsed-page)]
-    (assoc entry :page/data evaluated-page)))
+        evaluated-page (read/eval-all parsed-page)
+        page-metadata (page/lift-metadata evaluated-page {})
+        hiccup-page [:html (page/doc-header page-metadata)
+                     [:body
+                      [:main
+                       (apply conj
+                              [:article {:lang "en-us"}]
+                              (page/parse-paragraphs evaluated-page))]
+                      [:footer [:div [:a {:href "/"} "Home"]]]]]]
+    (assoc entry :site.fabricate.page/data hiccup-page)))
 
-(defmethod assemble [:fabricate/v1 :hiccup]
+(defmethod assemble [:fabricate/v0 :hiccup]
   [entry]
-  (fabricate-v1->hiccup entry))
+  (fabricate-v0->hiccup entry))
 
-(def assemble-index nil)
+;; (def assemble-index nil)
 
-(defmethod assemble "index.html" [entry] (assemble-index entry))
+;; (defmethod assemble "index.html" [entry] (assemble-index entry))
+
+(defn- output-path
+  [input-file output-location]
+  (cond (fs/directory? output-location)
+        (fs/file (str output-location "/" (fs/file-name input-file)))
+        (instance? java.io.File output-location) output-location))
+
+(defmulti produce!
+  "Generate output from an entry's page data and write it to the designated output file."
+  {:term/definition
+   {:source (URI. "https://www.merriam-webster.com/dictionary/produce"),
+    :definition
+    "to make available for public exhibition or dissemination; to cause to have existence or to happen; to give being, form, or shape to; to compose, create, or bring out by intellectual or physical effort; to bear, make, or yield something"}}
+  (fn [entry] [(:site.fabricate.page/format entry)
+               (get-in entry
+                       [:site.fabricate.page/output
+                        :site.fabricate.page/output-format])]))
+
+(defn write-hiccup-html!
+  "Generate HTML from Hiccup data and write it to the given file."
+  [hiccup-page-data output-file]
+  (spit output-file (hiccup.page/html5 hiccup-page-data)))
+
+(defmethod produce! [:hiccup :html]
+  [{:keys [site.fabricate.page/data site.fabricate.page/output
+           site.fabricate.page/input-file],
+    :as entry}]
+  (let [output-file (fs/file (str (output-path
+                                   input-file
+                                   (:site.fabricate.page/output-location
+                                    output))
+                                  ".html"))]
+    (write-hiccup-html! data output-file)
+    (-> entry
+        (dissoc :site.fabricate.page/output)
+        (assoc :site.fabricate.page/output-file output-file
+               :site.fabricate.page/output-format :html))))
 
 (comment
-  (plan {:input-dir "pages", :patterns patterns}))
+  (produce! (assemble (first (plan {:patterns patterns, :input-dir "pages"}))))
+  (meta #'assemble)
+  (meta assemble))
--- test/site/fabricate/prototype/api_test.clj
+++ test/site/fabricate/prototype/api_test.clj
@@ -6,9 +6,27 @@
 (t/deftest entries
   (let [entry-data (api/plan {:patterns api/patterns, :input-dir "pages"})]
     (t/testing "planning"
-      (t/is (every? #(instance? java.io.File (:site.fabricate/input-file %))
+      (t/is (every? #(instance? java.io.File
+                                (:site.fabricate.page/input-file %))
                     entry-data)))
     (t/testing "assembly"
       (doseq [e entry-data]
         (let [assembled (api/assemble e)]
-          (t/is (some? (:page/data assembled))))))))
+          (t/is (some? (:site.fabricate.page/data assembled))))))
+    (t/testing "production"
+      (let [tmp-dir (fs/create-temp-dir {:prefix "fabricate-test-"})]
+        (doseq [e entry-data]
+          (let [assembled (assoc-in (api/assemble e)
+                                    [:site.fabricate.page/output
+                                     :site.fabricate.page/output-dir]
+                                    tmp-dir)
+                {:keys [site.fabricate.page/output-file], :as produced}
+                (api/produce! assembled)]
+            (t/is (instance? java.io.File output-file))
+            (t/is (fs/exists? output-file))))))))
+
+(comment
+  (fs/create-temp-dir {:prefix "something-"})
+  (def assembled-posts-test
+    (mapv api/assemble (api/plan {:input-dir "pages", :patterns api/patterns})))
+  (first assembled-posts-test))
