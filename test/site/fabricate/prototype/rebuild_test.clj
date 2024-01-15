ba38948e35f1ca60a93dbe2e356f7e9272208271
Andrew Foltz-Morrison <thefomo@gmail.com>
2024-01-15 15:27:50 -0500

    Sketch of event loop for rebuild/draft mode
    This idea is still stabilizing, but it at least has a
    structured testing method

---
+++ test/site/fabricate/prototype/rebuild_test.clj
@@ -0,0 +1,61 @@
+(ns site.fabricate.prototype.rebuild-test
+  (:require [clojure.test :as t]
+            [site.fabricate.prototype.api :as api]
+            [site.fabricate.prototype.rebuild :refer :all])
+  (:import [java.util.concurrent LinkedBlockingQueue]))
+
+(def test-status (atom {:queue :waiting}))
+
+(def test-queue)
+
+(def ^:dynamic *test-process* nil)
+
+(defn test-fixture
+  [f]
+  (binding [*test-process* (future (while true
+                                     (swap! test-status update-state)))]
+    (f)
+    (future-cancel *test-process*)
+    (swap! test-status assoc :queue :terminated)))
+
+(t/use-fixtures :once test-fixture)
+
+(t/deftest actions
+  (let [test-events (atom [])]
+    ;; using a sequence of "expected actions" opens up the door to more
+    ;; expressive property checks in the future, such as:
+    ;; - 'the size of the list of actions performed cannot increase while
+    ;; the statue is :terminated or :waiting'
+    ;; - 'each action in a sequence of actions to be performed will occur
+    ;; exactly once or never'
+    ;; - etc
+    (t/testing "event loop state machine:"
+      (swap! test-status assoc :queue :running)
+      (t/testing "basic actions"
+        (let [_output (.put action-queue
+                            #(swap! test-events conj :test-action))]
+          (Thread/sleep 25) ;; is there a more principled way of doing
+          ;; this?
+          (t/is (= [:test-action] @test-events))
+          (t/is (= :running (:queue @test-status))
+                "Regular actions should run without errors")))
+      (t/testing "exceptions"
+        (let [_output (.put action-queue
+                            #(do (swap! test-events conj :test-error)
+                                 (throw (ex-info "manual exception"
+                                                 {:context :test}))))]
+          (Thread/sleep 25)
+          (t/is (= :waiting (:queue @test-status))
+                "Exceptions should pause event loop")
+          (t/is (= :test-error (peek @test-events))
+                "Error should be present in sequence of actions")
+          (.put action-queue #(do (swap! test-events conj :test-pause)))
+          (t/is (not (empty? (seq action-queue)))
+                "Actions enqueued while waiting should remain on the queue")
+          (swap! test-status assoc :queue :running)
+          (Thread/sleep 25)
+          (t/is
+           (= :test-pause (peek @test-events))
+           "Actions enqueued while waiting should be performed after events resume")
+          (t/is (false? (future-done? *test-process*))
+                "Process should be running at the end of the tests"))))))
