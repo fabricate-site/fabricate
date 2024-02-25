a612edef698f93525590b9d611dc53d187df032a
Andrew Foltz-Morrison <thefomo@gmail.com>
2024-02-25 10:02:19 -0500

    Ignore Unicode variation settings in template grammar
    When the MacOS keyboard inserts the ✳ emoji, it also inserts
    the 0xFE0F (variation selector 16) character, which is treated
    as the first form when the read namespace attempts to parse the
    expression in the template. Ignoring the whole range of
    variation selectors is the best way to ensure that the ✳
    character is treated consistently across platforms.

--- src/site/fabricate/prototype/read/grammar.clj
+++ src/site/fabricate/prototype/read/grammar.clj
@@ -2,38 +2,32 @@
   "Instaparse grammar for Fabricate's page templates."
   (:require [instaparse.core :as insta]))
 
-(comment (require '[lambdaisland.regal :as regal])
-
-         ;; the general idea here is :
-         ;; if the terminal is present, match all text before it
-         ;; (match without capturing)
-         ;; if it is not present, match all the text
-
-         ;; same regex matching on both, no char included
-         (re-seq #"^.*?(?=🔚|$)" "text with ending🔚")
-
-         (re-seq #"^.*?(?=🔚|$)" "text ")
-
-         ;; reluctant quantifiers are the key here
-
-         (re-seq #"^.*?(?=(?:🔚)|$)" "text with ending🔚")
-         (re-seq #"^.*?(?=(?:🔚|//🔚)|$)" "text with ending//🔚")
-
-         (re-seq #"^[^\s\S✳]*?(?=(?:🔚)|$)" "text with embedded ✳ and more txt")
-
-         (re-seq #"\A[^✳🔚]++|([\S\s]*?(?=(?:✳|/{2}?🔚)|\Z))"
-                 "text (with parens) and an expr ✳=(+ 3 4 5)🔚 and")
-
-         ((insta/parser "r = EPSILON
+(comment
+  (require '[lambdaisland.regal :as regal])
+  ;; the general idea here is : If the terminal is present, match all text
+  ;; before it
+  ;; (match without capturing)
+  ;; if it is not present, match all the text
+  ;; same regex matching on both, no char included
+  (re-seq #"^.*?(?=🔚|$)" "text with ending🔚")
+  (re-seq #"^.*?(?=🔚|$)" "text ")
+  ;; reluctant quantifiers are the key here
+  (re-seq #"^.*?(?=(?:🔚)|$)" "text with ending🔚")
+  (re-seq #"^.*?(?=(?:🔚|//🔚)|$)" "text with ending//🔚")
+  (re-seq #"^[^\s\S✳]*?(?=(?:🔚)|$)" "text with embedded ✳ and more txt")
+  (re-seq #"\A[^✳🔚]++|([\S\s]*?(?=(?:✳|/{2}?🔚)|\Z))"
+          "text (with parens) and an expr ✳=(+ 3 4 5)🔚 and")
+  ((insta/parser "r = EPSILON
                          w = #'\\s?$'") "\n")
-         (re-seq #"\s$" "\n")
-
-         (insta/parse
-          (insta/parser "rule = ( text | terminal | EPSILON ) *
+  (re-seq #"\s$" "\n")
+  (insta/parse
+    (insta/parser
+      "rule = ( text | terminal | EPSILON ) *
                          text = #'^[\\s\\S]*?(?=(?:/{2}?🔚)|$)$'
                          terminal = <#'/{2}?🔚'>")
-          "text with ending\n"
-          :trace true))
+    "text with ending\n"
+    :trace
+    true))
 
 (def ^:private txt-insta-regex
   "the left side of txt's regex is a fast possessive quantifier for the easy case, the right side is the more complex lookahead"
@@ -47,15 +41,14 @@
 (def template
   "The formal grammar for Fabricate templates."
   (insta/parser
-   (format
-    "template = EPSILON | ( expr | txt | extended-form )*
-    initial = '✳'
-    terminal = '🔚'
+    (format
+      "template = EPSILON | ( expr | txt | extended-form )*
+    initial = '✳' var-settings?
+    terminal = '🔚' var-settings?
+    (* Unicode range for variation settings used by Apple in the Private Use Area *)
+    var-settings = #'[\\uFE00-\\uFE0F]'
     ctrl = ('=' | '+' | '+=')
     expr = <initial> !'//'  ctrl?  #'[^=+][^🔚]*' !'//' <terminal>
-
-    (* the left side of txt's regex is a fast possessive quantifier
-       for the easy case, the right side is the more complex lookahead *)
     txt = #'%s'
 
     (* extended forms allow arbitrary nesting without breaking the flow *)
@@ -63,4 +56,4 @@
     extended-form = (<initial> <'//'> '[' #'[^\n✳🔚]*' <'\n'> form-contents ']' <'//'> <terminal>) |
                     (<initial> <'//'> '(' #'[^\n✳🔚]*' <'\n'> form-contents ')' <'//'> <terminal>) |
                     (<initial> <'//'> '{' #'[^\n✳🔚]*' <'\n'> form-contents '}' <'//'> <terminal>)"
-    txt-insta-regex)))
+      txt-insta-regex)))
