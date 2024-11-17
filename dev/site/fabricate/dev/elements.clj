f8ac0ea666adf01121c29aaa54e3dbed27c69f8c
Andrew Foltz-Morrison <thefomo@gmail.com>
2024-11-17 13:27:44 -0500

    Add default header and logo to index

--- dev/site/fabricate/dev/elements.clj
+++ dev/site/fabricate/dev/elements.clj
@@ -15,3 +15,9 @@
     [:div {:class "footer-top"}
      [:a {:class "decor-internal" :href top-id} "Top"]]])
   ([] (footer "#top")))
+
+(def header-big
+  [:header {:class "header-big"}
+   [:img {:src "/media/fabricate-logo-v1.svg" :class "fabricate-header-logo"}]
+   [:h1 {:class "header-primary"} "Fabricate"]
+   [:h3 {:class "header-secondary"} "Form by art and labor"]])
