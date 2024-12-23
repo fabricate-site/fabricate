a908c6713b065453903f7c75c2b4880edcb5f56d
Respatialized <respatialized@users.noreply.github.com>
2024-12-23 11:11:22 -0500

    Fix file parsing and comment text extraction fns

--- src/site/fabricate/prototype/source/clojure.clj
+++ src/site/fabricate/prototype/source/clojure.clj
@@ -6,7 +6,8 @@
             [rewrite-clj.zip :as zip]
             [malli.core :as m]
             [babashka.fs :as fs]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 
 
@@ -24,17 +25,44 @@
 
 (def form-map-schema
   "Schema describing a simplified way of storing Clojure forms and their potential results."
-  (m/schema [:map
-             [:clojure/source
-              {:description "Source of the expression as a string"} :string]
-             [:clojure/form
-              {:description "Parsed Clojure expression (unevaluated)"} :any]
-             [:clojure.form/metadata
-              {:description "Metadata of form" :optional true} :map]
-             [:input/file {:description "Source file (relative to project)"}
-              [:fn fs/exists?]]
-             [:clojure/results
-              {:description "Results of evaluation" :optional true} :any]]))
+  (m/schema
+   [:map
+    [:clojure/source {:description "Source of the expression as a string"}
+     :string]
+    [:clojure/form
+     {:description "Parsed Clojure expression (unevaluated)" :optional true}
+     :any]
+    [:clojure.form/metadata {:description "Metadata of form" :optional true}
+     :map]
+    [:input/file {:description "Source file (relative to project)"}
+     [:fn fs/exists?]]
+    [:clojure/results {:description "Results of evaluation" :optional true}
+     :any]
+    [:clojure/comment {:description "Clojure comment as string" :optional true}
+     :string]
+    [:text
+     {:description "Clojure comment text, without leading semicolon"
+      :optional    true} :string]]))
+
+(defn normalize-node
+  "Return a map representing the 'value' for the given node"
+  [n]
+  (let [t (node/tag n)]
+    (case t
+      :comment    {:clojure/comment (str n) :text "" :clojure/node n}
+      :newline    {:clojure/newlines (str n) :line-count 0 :clojure/node n}
+      :whitespace {:clojure/whitespace (str n) :clojure/node n}
+      (if (node/sexpr-able? n)
+        {:clojure/node n :clojure/expr (node/sexpr n)}
+        {:type :unknown :node/tag t :clojure/node n}))))
+
+
+(def ^:private comment-pattern #"(?:;+\s*)([\s\S]*)")
+
+(defn- extract-comment-text
+  [comment-str]
+  (last (re-matches comment-pattern comment-str)))
+
 
 (defn node->map
   "Convert the given rewrite-clj node into a form map."
@@ -43,10 +71,14 @@
                               (assoc mm (keyword "clojure.source" (name k)) v))
                             m
                             (meta n))
-        form     (when (node/sexpr-able? n) (node/sexpr n))]
+        form     (when (node/sexpr-able? n) (node/sexpr n))
+        comment? (node/comment? n)]
     (merge src-info
-           {:clojure/source (node/string n) :clojure/node n :clojure/form form}
-           (when (meta form) {:clojure.form/metadata (meta form)}))))
+           {:clojure/source (node/string n) :clojure/node n}
+           (when (meta form) {:clojure.form/metadata (meta form)})
+           (when form {:clojure/form form})
+           (when comment?
+             {:clojure/comment (str n) :text (extract-comment-text (str n))}))))
 
 (defn file->forms
   "Generate a sequence of Clojure form maps from the input file."
@@ -54,3 +86,7 @@
   [f]
   (let [parsed (parser/parse-file-all f)]
     (mapv #(node->map % {:input/file f}) (:children parsed))))
+
+(comment
+  (str (parser/parse-string ";; a comment"))
+  (file->forms *file*))
--- test/site/fabricate/prototype/source/clojure_test.clj
+++ test/site/fabricate/prototype/source/clojure_test.clj
@@ -2,16 +2,35 @@
   (:require [clojure.test :as t]
             [babashka.fs :as fs]
             [malli.core :as m]
+            [malli.error :as me]
             [site.fabricate.prototype.source.clojure :as clj]))
 
-(def valid-form-map? (m/validator clj/form-map-schema))
+
+
+(t/deftest extract-functions
+  (t/testing "clojure comments"
+    (t/is
+     (=
+      "in the assemble step, should the Clojure code be treated as a \"block\"?\n"
+      (#'clj/extract-comment-text
+       ";; in the assemble step, should the Clojure code be treated as a \"block\"?\n")))
+    (t/is (= "a comment " (#'clj/extract-comment-text ";; a comment ")))))
 
 (t/deftest parsing
-  (t/testing "parsing file into sequence of forms with rewrite-clj"
-    (doseq [src-file (fs/glob "src" "**.clj")]
-      (let [forms (clj/file->forms (fs/file src-file))]
-        (t/is (every? valid-form-map? forms))))))
+  (t/testing "comment extraction")
+  (let [valid-form-map? (m/validator clj/form-map-schema)
+        form-explainer  (m/explainer clj/form-map-schema)]
+    (t/testing "parsing file into sequence of forms with rewrite-clj"
+      (doseq [src-file (fs/glob "." "**.clj")]
+        (t/testing (str "\n" src-file)
+          (let [forms      (clj/file->forms (fs/file src-file))
+                all-valid? (every? valid-form-map? forms)]
+            (when-not all-valid?
+              (doseq [invalid-form (filter #(not (valid-form-map? %)) forms)]
+                (println (me/humanize (form-explainer invalid-form)))))
+            (t/is all-valid? "Each parsed form should be valid.")))))))
 
 
 (comment
+  clojure.java.io/do-copy
   print-dup)
