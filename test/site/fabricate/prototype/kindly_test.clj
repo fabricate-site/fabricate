96da7a91d58f2822416a8e1a892087f1abe66c97
Respatialized <respatialized@users.noreply.github.com>
2025-08-13 11:32:21 -0400

    Revert "Rewrite specification of Kindly values to use mutual recursion"
    This specification is out of scope for Fabricate. Please see
    https://github.com/respatialized/kindly-spec for the more complex (and
    still broken) data model.
    
    This reverts commit 1b335d0e43e4dcd09473c7c84a4cb45f69bc53a0.

--- test/site/fabricate/prototype/kindly_test.clj
+++ test/site/fabricate/prototype/kindly_test.clj
@@ -1,19 +1,16 @@
 (ns site.fabricate.prototype.kindly-test
   (:require [site.fabricate.prototype.kindly :as kindly]
-            [scicloj.kindly.v4.kind :as kind]
             [malli.core :as m]
             [clojure.test :as t]))
 
-(def example-nested-value
-  ^{:kind :fragment}
-  [^{:kind :hiccup}
-   [:div {:class "example"}
-    "An example Hiccup div containing a nested kindly element"
-    (kind/md "Nested **markdown** text in a Hiccup element")]
-   ^{:kind :code} '(+ 3 4 5 6)
-   {:kind :md :value "_More_ markdown text inside of a Kindly map."}
-   ^{:kind :edn} {:a 1 :b 2 :description "EDN map"}])
+
 
 (t/deftest data-model
   (t/testing "kindly"
-    (t/is (m/validate kindly/Kindly-Value example-nested-value))))
+    (t/is (m/validate kindly/Form
+                      {:code  "(into [:div] (for [i (range 9)] [:p i]))"
+                       :form  '(kind/hiccup
+                                (into [:div] (for [i (range 9)] [:p i])))
+                       :value ^{:kindly/kind :kind/hiccup}
+                              (into [:div] (for [i (range 9)] [:p i]))
+                       :kind  :kind/hiccup}))))
