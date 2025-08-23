98e0028872be3809038710d7c379cfa80a961f12
Respatialized <respatialized@users.noreply.github.com>
2025-08-23 09:41:22 -0400

    Improve provenance tracking and dynamism of evaluation context
    This ensures that any metadata set on a Kindly form that might indicate
    provenance will get added to a var, and that metadata resulting from
    evaluation (such as :doc) will get preserved in the form map returned after evaluation.

--- src/site/fabricate/prototype/eval.clj
+++ src/site/fabricate/prototype/eval.clj
@@ -1,16 +1,47 @@
 (ns site.fabricate.prototype.eval
   "Prototype namespace for evaluating Clojure code and forms."
   (:require [site.fabricate.prototype.kindly]
-            [malli.core :as m]))
+            [malli.core :as m]
+            [clojure.set :as set])
+  (:import [clojure.lang IObj]))
+
+
+;; I think it makes sense to move the schemas for parsed and evaluated
+;; expressions to this namespace rather than keep them in the read namespace.
+
+;; That way, this namespace can be the source of truth for what keys should and
+;; shouldn't be expected in the course of evaluating Kindly form maps. It can
+;; also be where the error messages are handled in more detail.
 
 ;; TODOs: namespace support (?)
+
+#_(def Evaluated-Form
+    (-> site.fabricate.prototype.kindly/Form
+        (mu/merge [:map
+                   [:error
+                    {:optional true :description "Result of Throwable->map"}
+                    :map] [:file]])))
+
+
+
 (defn eval-form
   "Evaluate the form in the given Map."
   ;; TODO: refine the schema to reflect pre and post-eval keys
   {:malli/schema (m/schema [:-> site.fabricate.prototype.kindly/Form
                             site.fabricate.prototype.kindly/Form])}
-  [{:keys [form value] :as form-map}]
+  [{:keys [form value ns] :as form-map}]
   (if (contains? form-map :value)
     form-map
-    (try (let [result (eval form)] (assoc form-map :value result))
+    (try (let [result (binding [*ns* (or ns *ns*)] (eval form))]
+           (if (instance? IObj result)
+             (let [result-meta     (meta result)
+                   new-result-meta (-> form-map
+                                       (set/rename-keys {:file/start-line :line
+                                                         :file/start-column
+                                                         :column})
+                                       (select-keys [:ns :file :line :column]))]
+               (alter-meta! result merge new-result-meta)))
+           (merge form-map
+                  {:value result}
+                  (when (meta result) {:meta (meta result)})))
          (catch Exception e (assoc form-map :error (Throwable->map e))))))
--- test/site/fabricate/prototype/eval_test.clj
+++ test/site/fabricate/prototype/eval_test.clj
@@ -1,22 +1,30 @@
 (ns site.fabricate.prototype.eval-test
   (:require [site.fabricate.prototype.eval :as prototype.eval]
             [site.fabricate.adorn :as adorn]
+            [matcher-combinators.test]
+            [matcher-combinators.matchers :as matchers]
             [clojure.test :as t]))
 
 (t/deftest evaluation
   (t/testing ": single exprs"
-    (t/is (= 5
-             (:value (prototype.eval/eval-form {:code "(+ 2 3)"
-                                                :kindly/hide-code true
-                                                :kindly/options {:hide-value
-                                                                 false}
-                                                :form (+ 2 3)}))))
-    (t/is (= {:form           '(+ 2 3)
-              :code           "(+ 2 3)"
-              :value          5
-              :kindly/hide-code true
-              :kindly/options {:hide-value false}}
-             (prototype.eval/eval-form {:code "(+ 2 3)"
-                                        :kindly/hide-code true
-                                        :kindly/options {:hide-value false}
-                                        :form '(+ 2 3)})))))
+    (t/is (match? 5
+                  (:value (prototype.eval/eval-form {:code "(+ 2 3)"
+                                                     :kindly/hide-code true
+                                                     :kindly/options
+                                                     {:hide-value false}
+                                                     :form (+ 2 3)}))))
+    (t/is (match? {:form           '(+ 2 3)
+                   :code           "(+ 2 3)"
+                   :value          5
+                   :kindly/hide-code true
+                   :kindly/options {:hide-value false}}
+                  (prototype.eval/eval-form {:code "(+ 2 3)"
+                                             :kindly/hide-code true
+                                             :kindly/options {:hide-value false}
+                                             :form '(+ 2 3)})))
+    (t/testing "custom namespaces"
+      (let [test-ns (create-ns 'eval-test-ns)]
+        (t/is (match?
+               {:form '(def test-var {:a :b}) :ns test-ns :meta {:ns test-ns}}
+               (prototype.eval/eval-form {:form '(def test-var {:a :b})
+                                          :ns   test-ns})))))))
