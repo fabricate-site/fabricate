98a96d13008cf6dc5437a9f39a0b79e91b5ba94b
Respatialized <respatialized@users.noreply.github.com>
2025-09-25 08:57:52 -0400

    Fix metadata handling in eval namespace

--- test/site/fabricate/prototype/eval_test.clj
+++ test/site/fabricate/prototype/eval_test.clj
@@ -12,7 +12,7 @@
                                                      :kindly/hide-code true
                                                      :kindly/options
                                                      {:hide-value false}
-                                                     :form (+ 2 3)}))))
+                                                     :form '(+ 2 3)}))))
     (t/is (match? {:form           '(+ 2 3)
                    :code           "(+ 2 3)"
                    :value          5
@@ -27,4 +27,13 @@
         (t/is (match?
                {:form '(def test-var {:a :b}) :ns test-ns :meta {:ns test-ns}}
                (prototype.eval/eval-form {:form '(def test-var {:a :b})
-                                          :ns   test-ns})))))))
+                                          :ns   test-ns})))))
+    (t/testing "metadata after evaluation"
+      ;; TODO: should Fabricate add its own metadata to trace provenance?
+      (let [var-form       {:code "(def my-test-var \"test value\")"
+                            :form '(def my-test-var "test value")
+                            :ns   (create-ns
+                                   'site.fabricate.prototype.eval.ephemeral)}
+            evaluated-form (prototype.eval/eval-form var-form)]
+        (println (meta (:value evaluated-form)))
+        (t/is (match? (:ns var-form) (:ns (meta (:value evaluated-form)))))))))
