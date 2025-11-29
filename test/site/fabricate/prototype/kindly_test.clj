54b4ec80a2332e45000df8e4895ca92b0cb6fe49
Respatialized <respatialized@users.noreply.github.com>
2025-11-29 11:23:48 -0500

    [WIP] Add one basic kindly-advice test and one failing test

--- test/site/fabricate/prototype/kindly_test.clj
+++ test/site/fabricate/prototype/kindly_test.clj
@@ -18,3 +18,28 @@
                        :kind  :kind/hiccup}))))
 
 (t/deftest functions (t/is (k/wrapped-value? (kind/code 3))))
+
+
+(t/deftest kindly-normalization
+  (require '[scicloj.kindly-advice.v1.api :as kindly-advice])
+  (require '[scicloj.kindly-advice.v1.advisors :as advisors])
+  (t/testing "kindly-advice"
+    (t/testing "general capabilities"
+      (let [nested-kindly-form
+            ^{:kindly/kind :kind/hiccup}
+            [:div {:class "basic-hiccup"} [:p "General paragraph text"]
+             [:figure
+              [:div
+               ^{:kindly/kind :kind/vega-lite}
+               {:encoding {:y    {:field "y" :type "quantitative"}
+                           :size {:value 400}
+                           :x    {:field "x" :type "quantitative"}}
+                :mark     {:type "circle" :tooltip true}
+                :data     {:values "x,y\n1,1\n2,-4\n3,9\n"
+                           :format {:type "csv"}}}]
+              [:figcaption
+               "Custom data visualization using nested Kindly forms"]]]]
+        (t/is (map? (kindly-advice/advise {:value nested-kindly-form}))
+              "Recursive normalization should work")))
+    (t/testing "fabricate compatibility"
+      (t/is false "Kindly-advice should recursively normalize forms"))))
